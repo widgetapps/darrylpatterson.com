c2b6b21afb053290b6d230ad0562b11d83e28bcb
Darryl Desktop <widgetapps@gmail.com>
2021-12-01 19:58:47 -0500

    Working on the big list.

--- pages/thelist.js
+++ pages/thelist.js
@@ -3,9 +3,13 @@ let listItems = require('../data/list.json');
 export default function List() {
     return (
         <div>
-            {listItems.map(listItem =>
-                <div>{listItem.title}</div>
-            )}
+            <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                {listItems.map(listItem =>
+                    <li key={listItem.id} className="col-span-1 flex shadow-sm rounded-md">
+                        {listItem.company}
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
