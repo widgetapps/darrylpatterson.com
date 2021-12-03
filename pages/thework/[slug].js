84d6b422dd58cfe397e953dedbb67e75a90386a5
Darryl Desktop <widgetapps@gmail.com>
2021-12-03 15:33:42 -0500

    Making progress in the work. Need to source all the content now.

--- pages/thework/[slug].js
+++ pages/thework/[slug].js
@@ -1,4 +1,7 @@
 import { useRouter } from 'next/router'
+import Image from "next/image";
+import Link from 'next/link';
+import {ArrowSmLeftIcon} from "@heroicons/react/solid";
 
 let works = require('../../data/work.json');
 
@@ -23,7 +26,86 @@ const WorkItem= () => {
     const {slug} = router.query;
     const work = works.find(item => item.slug.toString() === slug);
 
-    return <p>Post title: {work.title}</p>
+    return (
+        <div>
+            <div className="mb-3">
+                <a className="relative text-sm text-gray-700 font-medium hover:text-gray-500" href="/thework">
+                    <ArrowSmLeftIcon className="inline w-5 h-5 mb-1" aria-hidden="true" />
+                    <span className="">The Work</span>
+                </a>
+            </div>
+            <div className="grid grid-cols-4 gap-4 sm:gap-10">
+                <div>
+                    <div className="">
+                        <div>
+                            <img className="rounded-lg" src={work.thumbnail.src} alt={work.thumbnail.caption}/>
+                        </div>
+                        <div className="mt-10">
+                            {work.images.map(image =>
+                                <div className="">
+                                    <img className="w-32 h-32" className="" src={image.src} alt={image.caption}/>
+                                    <div className="text-xs">{image.caption}</div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+                <div className="col-span-3">
+                    <table className="mb-10">
+                        <tbody>
+                            <tr>
+                                <td className="w-24">The Work:</td>
+                                <td>{work.title}</td>
+                            </tr>
+                            <tr>
+                                <td>The Client:</td>
+                                <td>{work.client}</td>
+                            </tr>
+                            <tr>
+                                <td>The Role:</td>
+                                <td>{work.role}</td>
+                            </tr>
+                            <tr>
+                                <td>The When:</td>
+                                <td>{work.when}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div className="mb-10">
+                        <h2 className="text-xl font-bold">The Overview</h2>
+                        <div>{work.overview}</div>
+                    </div>
+
+                    <div className="mb-10">
+                        <h2 className="text-xl font-bold">The Links</h2>
+
+                        <table className="mb-10">
+                            <tbody>
+                                {work.links.map(link =>
+                                    <tr>
+                                        <td className="w-24">{link.type}:</td>
+                                        <td><Link href={link.url}>{link.link}</Link></td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div>
+                        <h2 className="text-xl font-bold mb-8">TL;DR</h2>
+                        {work.tldr.map(tldr =>
+                            <div className="mb-6">
+                                <h3 className="font-bold">{tldr.header}</h3>
+                                <div>{tldr.content}</div>
+                            </div>
+                        )}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default WorkItem
--- pages/thework/index.js
+++ pages/thework/index.js
@@ -1,37 +1,68 @@
 import Link from 'next/link';
 import React from "react";
+import {TemplateIcon} from "@heroicons/react/outline";
 let works = require('../../data/work.json');
 
 export default function Work() {
     return (
         <div>
             <div className="bg-head-work bg-center bg-cover flex-grow h-44 sm:rounded-lg"/>
-            {works.map(work =>
-                <Link href={`/thework/${work.slug}`}>{work.title}</Link>
-            )}
+            <div className="ml-10 mr-10 mb-8 sm:mb-14 mt-5 sm:mt-10 text-sm sm:text-lg">
+                I've been lucky to work on some really great projects and with some really great people. The roles are quite
+                fluid from one to the other - engineering, UX design, leadership, strategy, prototyping, branding, research.
+                Here are some of my favourites.
+            </div>
+            <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {works.map((work) => (
+                    <li
+                        key={work.id}
+                        className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+                    >
+                        <div className="flex-1 flex flex-col p-8">
+                            <img className="rounded-lg mx-auto" src={work.thumbnail.src} alt={work.thumbnail.caption} />
+                            <h3 className="mt-6 text-gray-900 text-sm font-medium">{work.title}</h3>
+                            <dl className="mt-1 flex-grow flex flex-col justify-between">
+                                <dt className="sr-only">Title</dt>
+                                <dd className="text-gray-500 text-sm">
+                                    {work.client}<br/>
+                                </dd>
+                            </dl>
+                            <dl className="mt-1 flex-grow flex flex-col justify-between">
+                                <dt className="sr-only">Title</dt>
+                                <dd className="text-gray-500 text-sm">
+                                    <div className="font-bold">Role:</div>
+                                    {work.role}
+                                </dd>
+                            </dl>
+                        </div>
+                        <div>
+                            <div className="-mt-px flex divide-x divide-gray-200">
+                                <div className="w-0 flex-1 flex">
+                                    <a
+                                        href={`/thework/${work.slug}`}
+                                        className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                                    >
+                                        <TemplateIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+                                        <span className="ml-3">View Details</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
 
-
-/*
-MBNA Mobile Credit Card Application
+/* Work to add:
 TODAY Show Experience Builder
-Aga Khan Museum Website Launch
-Home Hardware Launch
 Karma Co-op
-ManuLife PRPP Application
 TVO Gamified Learning
 Aviva Broker Microsite
 Cogeco Everyday Heroes
-OneHome Launch
 HP ePrint Hub
-TD Report Missing Card
-DWS IoT Stack
-Rotary Convention HOC Tech
 Rotary Auction System
 Centennial College Web Cert Programs
 Merisel Sellutions
-Dapagee Branding
-
  */
