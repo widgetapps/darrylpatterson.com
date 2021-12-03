148dd1147d5877699ffa9c3be00634961be08911
Darryl Desktop <widgetapps@gmail.com>
2021-12-03 12:26:30 -0500

    Everything but the work looks good methinks.

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import iconLinkedin from "../public/linkedin.svg";
+import iconGithub from "../public/github.svg";
+import React from "react";
 
 export default function Contact() {
     return (
         <div>
             <div className="bg-head-contact bg-center bg-cover flex-grow h-44 sm:rounded-lg"/>
-            Contact
+            <div className="grid grid-cols-5 gap-6 ml-10 mt-10">
+                <div className="w-12">
+                    <Link href="https://github.com/widgetapps">
+                        <Image src={iconGithub}/>
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/widgetapps/">
+                        <Image src={iconLinkedin}/>
+                    </Link>
+                </div>
+                <div className="col-span-4 mr-10">
+                    <h1 className="text-2xl mb-5">Wanna make some awesome things together?</h1>
+                    <div className="text-lg">Darryl Patterson</div>
+                    <div>
+                       <Link href="mailto:darryl@pattersonpowered.com">darryl@pattersonpowered.com</Link>
+                    </div>
+                    <div>
+                        <Link href="tel:+14167866116">(416) 786-6116</Link>
+                    </div>
+                    <div className="mt-4">Sault Ste Marie, ON CANADA</div>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 
 import Image from 'next/image';
+import Link from 'next/link';
 
 import meHoliday from '../public/me/darryl_holiday.jpg';
 import meGeeq from '../public/me/darryl_geeq.jpg';
 import meSailor from '../public/me/darryl_sailor.jpg';
 import meSuspicious from '../public/me/darryl_suspicious.jpg';
 import meViking from '../public/me/darryl_viking.jpg';
-import ListItem from "../components/listitem";
+
+import iconLinkedin from '../public/linkedin.svg';
+import iconGithub from '../public/github.svg';
+
 import React from "react";
 
 import homeContent from '../data/home.json';
@@ -51,7 +55,8 @@ function classNames(...classes) {
 export default function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 sm:gap-10">
-      <div className="-space-y-1.5 gap-0">
+      <div>
+          <div className="-space-y-1.5 gap-0">
           {mePhotos.map(photo =>
               <div className="">
                 <Image className={classNames(
@@ -64,6 +69,7 @@ export default function Home() {
                 )} src={photo.src} alt={photo.caption}/>
               </div>
           )}
+          </div>
       </div>
       <div className="col-span-3">
           <div className="text-xl sm:text-3xl italic mt-2 sm:mt-5 mb-8 sm:mb-14 mr-4 sm:mr-1">"Equal parts expert, educator and leader, my childhood fascination with the creative potential of
