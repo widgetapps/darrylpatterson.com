e35b7ca263ab2e6d5454b05f03a7d2ed6d7f0c80
widgetapps <widgetapps@gmail.com>
2024-08-01 13:51:05 -0400

    Updated the portfolio message.

--- pages/thework/index.js
+++ pages/thework/index.js
@@ -57,9 +57,8 @@ export default function Work() {
                 ))}
             </ul>
             <div className="ml-10 mr-10 mb-8 sm:mb-14 mt-5 sm:mt-10 text-sm sm:text-lg text-center">
-                Kindly note that my work is subject to strict NDAs and as a result, I cannot post a detailed hi-rez portfolio or
-                provide one in a format that I can send out. If you feel I'm a good fit, I'm more than happy to show you
-                my work during an interview.
+                Kindly note that my work is subject to strict NDAs and as a result, I cannot publicly post a portfolio
+                of recent work. A password protected Figma portfolio of my recent work is available upon request.
             </div>
         </div>
     )
